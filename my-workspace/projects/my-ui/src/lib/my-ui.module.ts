252956275a01ffb867b9908cc7938c650db574ae
yamashita-kenngo <yamashita-kenngo@outlook.jp>
2020-07-30 22:06:23 +0900

    初期準備

---
+++ my-workspace/projects/my-ui/src/lib/my-ui.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MyUiComponent } from './my-ui.component';
+
+
+
+@NgModule({
+  declarations: [MyUiComponent],
+  imports: [
+  ],
+  exports: [MyUiComponent]
+})
+export class MyUiModule { }
